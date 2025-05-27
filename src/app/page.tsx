2d486ef3bb2fc6b045c060aa3b1440a27afcb3d6
bishojoism <bishojoism@163.com>
2025-05-27 14:41:24 +0800

    完工

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,15 @@
 'use client'
 
-import { AppBar, Backdrop, Box, Button, ButtonGroup, CircularProgress, Container, IconButton, Stack, TextField, Toolbar, Typography } from "@mui/material";
+import { AppBar, Button, ButtonGroup, Container, IconButton, Link, Stack, TextField, Toolbar, Typography } from "@mui/material";
 import { GitHub } from "@mui/icons-material"
 import { useState } from "react";
-import { MuiFileInput } from "mui-file-input";
 import { decode256to64, encode64to256 } from "./base";
 import { saveAs } from "file-saver";
 
 export default function Home() {
-  const [loading, setLoading] = useState(false)
-  const [file, setFile] = useState<File | null>(null)
   const [text, setText] = useState('')
-  const upload = async (proxy: boolean) => {
-    if (!file) return
-    setLoading(true)
-    try {
-      const body = new FormData()
-      body.append('reqtype', 'fileupload')
-      body.append('fileToUpload', file)
-      const url = 'https://catbox.moe/user/api.php'
-      const res = await fetch(proxy ? `/api?url=${encodeURIComponent(url)}` : url, { method: 'POST', body })
-      const text = await res.text()
-      if (!res.ok) throw text
-      setText(encode64to256(text.substring(25)))
-    } catch (e) {
-      alert(e)
-    }
-    setLoading(false)
-  }
-  const download = async (proxy: boolean) => {
-    const filename = decode256to64(text)
-    const url = `https://files.catbox.moe/${filename}`
-    saveAs(proxy ? `/api?url=${encodeURIComponent(url)}` : url, filename)
-  }
   return (
     <>
-      <Backdrop
-        sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 1 })}
-        open={loading}
-      >
-        <CircularProgress color="inherit" />
-      </Backdrop>
       <AppBar>
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
@@ -54,38 +23,26 @@ export default function Home() {
       <Toolbar />
       <Container sx={{ paddingY: 3 }}>
         <Stack spacing={2}>
-          <Box display="flex" alignItems="center">
-            <MuiFileInput
-              label="选择或拖拽文件"
-              value={file}
-              onChange={setFile}
-              sx={{ width: 256 }}
-            />
-            <ButtonGroup disabled={!file}>
-              <Button onClick={() => upload(true)}>
-                免魔法上传
-              </Button>
-              <Button onClick={() => upload(false)}>
-                原接口上传
-              </Button>
-            </ButtonGroup>
-          </Box>
-          <Box display="flex" alignItems="center">
-            <TextField
-              label="乾坤文"
-              value={text}
-              onChange={event => setText(event.target.value)}
-              sx={{ width: 256 }}
-            />
-            <ButtonGroup disabled={!text}>
-              <Button onClick={() => download(true)}>
-                免魔法下载
-              </Button>
-              <Button onClick={() => download(false)}>
-                原接口下载
-              </Button>
-            </ButtonGroup>
-          </Box>
+          <Typography>
+            如何制作乾坤文：将文件上传至<Link href="https://catbox.moe">Catbox</Link>，将生成的链接复制粘贴至本页面下方的文本框内，点击“制作乾坤文”按钮。
+          </Typography>
+          <Typography>
+            如何使用乾坤文：将乾坤文复制粘贴至本页面下方的文本框内，点击“使用乾坤文”按钮。
+          </Typography>
+          <ButtonGroup>
+            <Button onClick={() => {
+              setText(encode64to256(text.substring(25)))
+            }}>
+              制作乾坤文
+            </Button>
+            <Button onClick={() => {
+              const filename = decode256to64(text)
+              saveAs(`/api?url=${encodeURIComponent(`https://files.catbox.moe/${filename}`)}`, filename)
+            }}>
+              使用乾坤文
+            </Button>
+          </ButtonGroup>
+          <TextField autoFocus value={text} onChange={event => setText(event.target.value)} />
         </Stack>
       </Container>
     </>

68aca85c850d0f72372aa43114ba7dde8cace15f
bishojoism <bishojoism@163.com>
2025-05-27 23:50:42 +0800

    魔法

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,10 +41,10 @@ export default function Home() {
       <Container sx={{ paddingY: 3 }}>
         <Stack spacing={2}>
           <Typography>
-            如何使用乾坤文：将乾坤文复制粘贴至“乾坤文”文本框内，普通下载链接将自动显示于“本站链接”后面，如果是主流图片格式（JPEG、PNG、BMP、GIF、TIFF、WebP、PDF、SVG），高速下载链接将自动显示于“别站链接（推荐）”后面。
+            如何使用乾坤文（无需魔法）：将乾坤文复制粘贴至“乾坤文”文本框内，普通下载链接将自动显示于“本站链接”后面，如果是主流图片格式（JPEG、PNG、BMP、GIF、TIFF、WebP、PDF、SVG），高速下载链接将自动显示于“别站链接（推荐）”后面。
           </Typography>
           <Typography>
-            如何制作乾坤文：将文件上传至<Link href="https://catbox.moe">Catbox</Link>，将生成的链接复制粘贴至“Catbox链接”文本框内，乾坤文将自动显示于“乾坤文”文本框内。建议先点击别站链接让其进行缓存，加速用户的查看。
+            如何制作乾坤文（需要魔法）：将文件上传至<Link href="https://catbox.moe">Catbox</Link>，将生成的链接复制粘贴至“Catbox链接”文本框内，乾坤文将自动显示于“乾坤文”文本框内。建议先点击别站链接让其进行缓存，加速用户的查看。
           </Typography>
           <TextField label="Catbox链接" value={url} onChange={event => setUrl(event.target.value)} />
           <TextField label="乾坤文" value={text} onChange={event => setText(event.target.value)} />

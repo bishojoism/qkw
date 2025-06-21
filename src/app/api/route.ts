9c7474432a19ac184ab7b8f82649e666783955dd
bishojoism <bishojoism@163.com>
2025-06-21 23:22:32 +0800

    调试

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -15,7 +15,7 @@ async function proxy(req: Request) {
         });
 
         const newHeaders = new Headers(res.headers);
-        newHeaders.set('Access-Control-Allow-Origin', 'https://saozi5.netlify.app');
+        newHeaders.set('Access-Control-Allow-Origin', 'https://localhost:5713');
 
         return new Response(res.body, {
             status: res.status,
